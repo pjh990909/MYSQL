b735c2f6856b617e2b4f902b24ce55f4f4769719
pjh990909 <pjh990909@naver.com>
2024-01-29 01:03:00 +0900

    1

--- Practice/Practice05.sql
+++ Practice/Practice05.sql
@@ -25,11 +25,35 @@ and commission_pct is null
 select  employee_id 직원번호,
 		first_name 이름,
         salary 월급,
-        hire_date 입사일,
-        phone_number 전화번호,
+        case when date_format(hire_date,'%Y년 %m월 %d일%W') = date_format(hire_date,'%Y년 %m월 %d일Monday') then date_format(hire_date,'%Y년 %m월 %d일(월요일)')
+			when date_format(hire_date,'%Y년 %m월 %d일%W') = date_format(hire_date,'%Y년 %m월 %d일Tuesday') then date_format(hire_date,'%Y년 %m월 %d일(화요일)')
+            when date_format(hire_date,'%Y년 %m월 %d일%W') = date_format(hire_date,'%Y년 %m월 %d일Wednesday') then date_format(hire_date,'%Y년 %m월 %d일(수요일)')
+            when date_format(hire_date,'%Y년 %m월 %d일%W') = date_format(hire_date,'%Y년 %m월 %d일Thursday') then date_format(hire_date,'%Y년 %m월 %d일(목요일)')
+            when date_format(hire_date,'%Y년 %m월 %d일%W') = date_format(hire_date,'%Y년 %m월 %d일Friday') then date_format(hire_date,'%Y년 %m월 %d일(금요일)')
+            when date_format(hire_date,'%Y년 %m월 %d일%W') = date_format(hire_date,'%Y년 %m월 %d일Saturday') then date_format(hire_date,'%Y년 %m월 %d일(토요일)')
+			when date_format(hire_date,'%Y년 %m월 %d일%W') = date_format(hire_date,'%Y년 %m월 %d일Sunday') then date_format(hire_date,'%Y년 %m월 %d일(일요일)')
+        end '입사일',
+        replace(phone_number,'.','-') 전화번호,
         department_id 부서번호
 from employees
-where (department_id,salary,hire_date) in (select department_id,max(salary),case when date_format(hiredate,%Y-%m-%d %a)
+where (department_id,salary) in (select department_id,max(salary)
 								from employees
                                 group by department_id)
-;
+order by salary desc;
+
+/*문제3
+매니저별 담당직원들의 평균월급 최소월급 최대월급을 알아보려고 한다.
+-통계대상(직원)은 2005년 이후(2005년 1월 1일 ~ 현재)의 입사자 입니다.
+-매니저별 평균월급이 5000이상만 출력합니다.
+-매니저별 평균월급의 내림차순으로 출력합니다.
+-매니저별 평균월급은 소수점 첫째자리에서 반올림 합니다.
+-출력내용은 매니저아이디, 매니저이름(first_name), 매니저별평균월급, 매니저별최소월급, 매니저별최대월급 입니다.
+(9건)
+*/
+
+select *
+from employees e, employees m,(select manager_id,avg(salary) avgsalary
+							   from employees
+                               group by manager_id) p
+where e.employee_id = p.manager_id
+and
